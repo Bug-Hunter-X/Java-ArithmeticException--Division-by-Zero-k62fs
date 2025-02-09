caa6b0b45cc0a37527497369938feff6b7b73d96
BugHunter-X <askzenai@gmail.com>
2025-02-09 10:24:04 +0100

    Add bugSolution.java file

---
+++ bugSolution.java
@@ -0,0 +1,12 @@
+public class MyClass {
+    public static void main(String[] args) {
+        int i = 10;
+        int j = 0;
+        if (j == 0) {
+            System.out.println("Division by zero is not allowed.");
+        } else {
+            int k = i / j;
+            System.out.println(k);
+        }
+    }
+}
